e09d90c030823a39f5e69f01760486c6544e37bf
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-19 14:44:09 -0400

    task4b

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,5 @@
  * my first comment
 */
 int main(void)
-{
-    puts ("Programming is like building a multilingual puzzle");
-return (0);
-}
+{ puts("Programming is like building a multilingual puzzle");
+return (0);}
