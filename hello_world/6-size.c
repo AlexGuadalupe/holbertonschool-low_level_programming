faa8ee2515fd33c4488ddc957798dd4ef146010d
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-19 16:30:27 -0400

    task6b

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ long long int c;
 char d;
 float f;
 
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of char: %lu byte(s)\n"), (unsigned long)sizeof(d));
+printf("Size an int: %lu byte(s)\n"), (unsigned long)sizeof(a));
+printf("Size of long: %lu byte(s)\n"), (unsigned long)sizeof(b));
+printf("Size of long long: %lu byte(s)\n"), (unsigned long)sizeof(c));
+printf("Size of float: %lu byte(s)\n"), (unsigned long)sizeof(f));
 return (0);
 }
