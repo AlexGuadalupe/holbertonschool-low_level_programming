d6fab8edb53c1bd08170c263836ca049bdc8c88c
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-20 22:55:07 -0400

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - 
+*Return: 
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
