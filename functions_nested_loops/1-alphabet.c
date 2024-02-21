df6f16c2682d411fa2e2c2ff05f03b77aa82e424
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-21 12:02:50 -0400

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	print_alphabet();
+    _putchar('\n');
+
+	return (0);
+}
