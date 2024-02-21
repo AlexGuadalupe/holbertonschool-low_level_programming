28193cbaad8f5863548e68415b9a14f64049e9c9
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-20 23:05:19 -0400

    task5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,14 @@
 #include <stdio.h>
 /**
 *main - acces point
-*description: prints num 0 to 10
+*description: prints num 0 to 9
 *Return: always (0)
 */
 int main(void)
 {
-	int n;
-	for (n = 0; n <= 9; n++)
-	{
-		printf("%d", n);
-		printf("\n");
-	}
+int n;
+for (n = 0; n <= 9; n++)
+printf("%d", n);
+printf("\n");
 return (0);
 }
