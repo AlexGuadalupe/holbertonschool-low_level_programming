0aab5cca23857cf1a06830746a6ddb3f5696bf62
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-20 22:31:50 -0400

    task4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Print all the letters except q and e
+*Return: Always 0
+*/
+int main(void)
+{
+char a
+for (a = 'a'; a <= 'z'; c++)
+
+if (a != 'e' && a != 'z')
+putchar(c);
+putchar('\n');
+return (0);
+}
