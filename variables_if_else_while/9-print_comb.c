da8186f01839fddf9b6a1074b322ed7405155b89
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-21 00:02:37 -0400

    task9late

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,16 +7,20 @@
  */
 int main(void)
 {
-int nums;
-for (nums =0; nums <=9; nums++)
-{
-putchar((nums % 10) + '0');
-if (nums == 9)
-continue;
+	int nums;
 
-putchar(',');
-putchar(' ');
+	for (nums =0; nums <=9; nums++)
+{
+	putchar((nums % 10) + '0');
+
+	if (nums < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
+
+putchar('\n');
 
 return (0);
 }
