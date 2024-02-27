af7356afa21324665d71eaf28d12452fb33512db
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-27 14:17:24 -0400

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rev_string - check point
+* @s: function that reverses a string. 
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+char temp;
+int i, len, len1;
+
+len = 0;
+len1 = 0;
+
+while (s[len] != '\0')
+{
+len++;
+}
+
+len1 = len - 1;
+
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len1];
+s[len1--] = temp;
+}
+}
