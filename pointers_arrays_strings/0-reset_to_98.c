ca57673d493b9a76cdc1d460b0b8a1754641ffa8
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-27 10:32:21 -0400

    taks0b

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - updates the value it points to to 98.
- *
+ * reset_to_98- updates the value it points to to 98.
+ * @n: points to an integer
  * Return: void
 */
 void reset_to_98(int *n)
