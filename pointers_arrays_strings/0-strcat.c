a92ecd256923513c2220751d7cf917bd1d1cf3b7
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-28 10:22:43 -0400

    taks0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - appends the src string to the dest string
+ * @dest: string
+ * @src: string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int size = 0, j;
+
+while(dest[size])
+{
+size++;
+}
+
+for(j=0; j < size +1 && src[j]; j++)
+{
+dest[size++] = src[j];
+}
+
+return ('\0');
+}
