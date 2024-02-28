dff35dd3b53fc6cda63fa63f174bb99505a646e8
Alex Guadalupe <159042986+AlexGuadalupe@users.noreply.github.com>
2024-02-28 01:41:58 +0000

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - a funtion that takes a pointer to an int as paramater and
+ * @dest: chaine of character
+ * @src: chaine character
+ *
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int len, i;
+len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
