3282727e553e9e7f6ad81e587c0d8a04fe4d231b
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-28 20:52:14 -0400

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - appends src to the dest string
+ *@n:bytes from src
+ *@dest:string to apppend by src
+ *@src: string to append by test
+ *
+ * Return: addres of dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src && n > 0)
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+		n--;
+	}
+
+	*dest = '\0';
+	return (dest);
+}
