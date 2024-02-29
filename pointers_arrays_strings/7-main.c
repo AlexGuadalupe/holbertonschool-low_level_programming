d1d08cf0ef5af71fb849fdee7108aef2be5a12fd
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-28 23:56:53 -0400

    task7again

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,18 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for
+ * leet - Encodes a string into 1337
+ * @s: The string to be encoded
  *
- * Return: Always 0.
- */
-int main(void)
+ * Return: The encoded string
+*/
+char *leet(char *s)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
-
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
-    return (0);
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
+return (s);
 }
