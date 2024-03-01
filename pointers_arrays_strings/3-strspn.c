cf5c813d668d2b1d27cd7567ec46352b1e34069b
Alex Guadalupe <9035@holbertonstudents.com>
2024-02-29 22:37:25 -0400

    task3b

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,8 @@
  * int _strspn- function that gets the length of a prefix substring.
  * @s: string to be scanned.
  * @accept: string with the characters to match.
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: number of bytes in the initial segment of s which consist only of
+ * bytes from accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
