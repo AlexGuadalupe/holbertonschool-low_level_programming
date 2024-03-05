2d6d48bf3ce25eb3f926fefff9408b3870274fb2
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-05 16:26:50 -0400

    task3c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - main function
+ * @argc: arg count
+ * @argv: arg name
+ * Return: 0 if successful
+*/
 int main(int argc, char *argv[])
 {
 int a = atoi(argv[1]);
