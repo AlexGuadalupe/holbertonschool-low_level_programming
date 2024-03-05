e2f44aa54f3e231df5e309ec7741b8c6c9bcb147
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-05 13:20:34 -0400

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: arg count
+ * @argv: arg name
+ * Return: 0 if succesful
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc -1);
+return (0);
+}
