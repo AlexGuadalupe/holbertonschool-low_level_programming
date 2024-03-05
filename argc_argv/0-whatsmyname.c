97a37a0b773e1b864c8e853e968ce799347a859c
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-05 11:57:02 -0400

    tasks0-01

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- * main -
- * @argc:
- * @argv:
- * Return:
+ * main - main function
+ * @argc: arg count
+ * @argv: arg name
+ * Return: 0 if succesful
 */
-
 int main(int argc, char *argv[])
 {
 argc++;
 argc--;
-
 printf("%s\n", argv[0]);
-
 return (0);
 }
