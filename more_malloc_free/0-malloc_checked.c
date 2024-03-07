500d3495717bddc8c198b79ca01b0e76d56e3114
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-07 19:15:50 -0400

    task0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ *malloc_checked - allocates memory using malloc
+ *@b: unsigned int type
+ *Return: to pointer
+*
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *pointer;
+pointer = malloc(b);
+
+if (pointer == NULL)
+exit(98);
+return (pointer);
+}
