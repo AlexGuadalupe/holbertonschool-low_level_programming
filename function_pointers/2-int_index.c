eb34abb0c3d04e887a18ebd8fae106d1357b5168
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-13 21:59:07 -0400

    task2 fix1

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * array_iterator - iterator for array elements
+ * int_index - return index place if comparison = true else -1
  * @array: array
- * @size: size of array
- * @action: pointer to function
- * Return: void
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
+ * @size: size of elements in array
+ * @cmp: pointer to function of one of the 3 in main
+ * Return: 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
 {
-unsigned int i;
-if (array == NULL || action == NULL)
-return;
+int i;
+if (array == NULL || size <= 0 || cmp == NULL)
+return (-1);
 for (i = 0; i < size; i++)
 {
-action(array[i]);
+if (cmp(array[i]))
+return (i);
 }
+return (-1);
 }
