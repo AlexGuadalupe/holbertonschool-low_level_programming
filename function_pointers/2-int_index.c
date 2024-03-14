49c3617a3e04b04498c6f6a9cca7fb07857082a9
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-13 21:29:12 -0400

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - iterator for array elements
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
