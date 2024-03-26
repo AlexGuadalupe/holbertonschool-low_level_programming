a8c3963e5d07e7ce526e57e911c98b4bb160350c
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-25 23:30:08 -0400

    all tasks review

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+* add_node - returns a new link added to list
+* @head: double pointer to the head of a linked list.
+* @str: a string
+* Return: new link
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+char *dup;
+int len;
+list_t *new;
+
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+
+dup = strdup(str);
+if (dup == NULL)
+{
+free(new);
+return (NULL);
+}
+
+for (len = 0; str[len];)
+len++;
+
+new->str = dup;
+new->len = len;
+new->next = *head;
+
+*head = new;
+
+return (new);
+}
