e296cdfa5161553c8be88984410e49820aa4972a
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-25 22:54:00 -0400

    fix betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,26 +7,26 @@
 */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
+if (h == NULL)
+{
+return (0);
+}
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			count++;
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-			count++;
-		}
-		h = h->next;
-	}
-	return (count);
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+count++;
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+count++;
+}
+h = h->next;
+}
+return (count);
 }
